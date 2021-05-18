cec650a59421d37a4c4ed69984d8ccb6474a22ea
meloegel <meloegel@gmail.com>
2021-05-18 09:35:40 -0400

    heroku fix

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-
+const pgConnection = process.env.DATABASE_URL
 
 module.exports = {
   development: {
@@ -15,19 +15,21 @@ module.exports = {
     },
   },
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+    client: 'pg',
+    connection: pgConnection,
+    ssl: {
+      rejectUnauthorized: false
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   }
 
 };
